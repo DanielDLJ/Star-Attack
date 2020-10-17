64896ec05405476d5d660fdbae582fc40da82f8c
Daniel Leme Júnior <daniel.dlj@hotmail.com>
2020-10-17 00:06:10 -0300

    [ADD] Moving background image

---
+++ Assets/Scripts/BG Scroll Script/BGScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour{
+
+  public float scroll_Speed = 0.1f;
+  private MeshRenderer meshRenderer;
+  private float x_Scroll;
+
+  // Start is called before the first frame update
+  void Start(){
+    meshRenderer = GetComponent<MeshRenderer>();
+  }
+
+  // Update is called once per frame
+  void Update(){
+      Scroll();
+  }
+  void Scroll(){
+      x_Scroll = Time.time * scroll_Speed;
+
+      Vector2 offset = new Vector2(x_Scroll, 0f);
+      meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+  }
+}
