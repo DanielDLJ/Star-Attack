d5ca05c621802c90de57f9b8ef7736412be7db01
Daniel Leme Júnior <daniel.dlj@hotmail.com>
2020-10-17 20:02:04 -0300

    [ADD] Player Bullet

---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    public float speed = 5f;
+    public float min_Y, max_Y = 5f;
+
+    [SerializeField]
+    private GameObject player_Bullet;
+
+    [SerializeField]
+    private Transform attack_Point;
+
+    private float attack_Timer = 0.35f;
+    private float current_Attack_Timer;
+    private bool canAttack;
+
+    // Start is called before the first frame update
+    void Start(){
+        current_Attack_Timer = attack_Timer;
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        MovePlayer();
+        Attack();
+    }
+
+    void MovePlayer(){
+        if(Input.GetAxisRaw("Vertical") > 0f){
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+
+            if (temp.y > max_Y) temp.y = max_Y;
+            transform.position = temp;
+        }else if (Input.GetAxisRaw("Vertical") < 0f){
+            Vector3 temp = transform.position;
+            temp.y -= speed * Time.deltaTime;
+
+            if (temp.y < min_Y) temp.y = min_Y;
+            transform.position = temp;
+        }
+        
+    }
+
+    void Attack(){
+        attack_Timer += Time.deltaTime;
+        if(attack_Timer > current_Attack_Timer){
+            canAttack = true;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.K)){
+            if (canAttack){
+                canAttack = false;
+                attack_Timer = 0f;
+                Transform baseAmmoTransform = player_Bullet.GetComponentsInChildren<Transform>(true)[0];
+                Quaternion baseAmmoRotation = baseAmmoTransform.rotation;
+                Instantiate(player_Bullet, attack_Point.position, baseAmmoRotation);
+                //Instantiate(player_Bullet, attack_Point.position, Quaternion.identity);
+            }
+        }
+    }
+
+}
